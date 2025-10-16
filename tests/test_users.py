456fba86988b0c0724576132324c09bbd4ad94fb
Akshat <akshatjecrc@gmail.com>
2025-10-16 19:40:10 +0530

    full code

---
+++ tests/test_users.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_root():
+    res = client.get("/")
+    print(res.json().get('message'))
+    assert res.json().get('message') == 'Hello World'
+    assert res.status_code == 200
+
+def test_create_user():
+    res = client.post("/users/",json={"email":"hello123@gmail.com", "password":"password123"})
+    
